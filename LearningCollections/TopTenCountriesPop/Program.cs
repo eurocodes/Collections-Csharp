89e60171cb04e8e0b052cbab627a67749a178bf9
Ugo Umeoke <laryhug209@yahoo.com>
2021-04-09 09:56:56 +0100

    Allowed user to select how many country to display

--- LearningCollections/TopTenCountriesPop/Program.cs
+++ LearningCollections/TopTenCountriesPop/Program.cs
@@ -10,16 +10,28 @@ namespace LearningCollections.TopTenCountriesPop
             string filePath = @"C:\Users\hp\Desktop\Ugee\.NET\WEEK-2\Pluralsight\Projects\LearningCollections\Pop by Largest Final.csv";
             CsvReader reader = new CsvReader(filePath);
 
-            var countries = reader.ReadAllCountries();
-            Console.WriteLine("Which country code do you want to lookup?...");
-            string userinput = Console.ReadLine();
+            List<Country> countries = reader.ReadAllCountries();
+            Console.Write("enter number of countries to display ");
 
-            bool gotCountry = countries.TryGetValue(userinput, out Country country);
-            if (!gotCountry)
-                Console.WriteLine($"Sorry there is no country with country code, {userinput}");
-            else
-                Console.WriteLine($"{country.Name} has population " +
-                    $"{PopulationFormatter.FormatPopulation(country.Population)}");
+            bool inputIsint = int.TryParse(Console.ReadLine(), out int userInput);
+            if (!inputIsint || userInput <= 0)
+            {
+                Console.WriteLine("You must type a +ve integer. Exiting...");
+                return;
+            }
+            int maxToDisplay = userInput;
+            for (int i = 0; i < maxToDisplay; i++)
+            {
+                if (i > 0 && (i % maxToDisplay == 0))
+                {
+                    Console.WriteLine("Hit return to continue, anything else to quit > ..");
+                    if (Console.ReadLine() != "")
+                        break;
+                }
+                Country country = countries[i];
+                Console.WriteLine( $"{i + 1}: {PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}:" +
+                    $" {country.Name}");
+            }
 
             Console.WriteLine($"Total countries: {countries.Count}");
 
