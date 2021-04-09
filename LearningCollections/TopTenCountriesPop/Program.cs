0ee8e977664c94bf3b543dfbec9badc3e23614cf
Ugo Umeoke <laryhug209@yahoo.com>
2021-04-09 07:01:35 +0100

    Add project files.

---
+++ LearningCollections/TopTenCountriesPop/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningCollections.TopTenCountriesPop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\hp\Desktop\Ugee\.NET\WEEK-2\Pluralsight\Projects\LearningCollections\Pop by Largest Final.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            List<Country> countries = reader.ReadAllCountries();
+
+            // This is the code that inserts and then subsequently removes Lilliput.
+            // Comment out the RemoveAt to see the list with Lilliput in it.
+            Country lilliput = new Country("Lilliput", "LIL", "Somewhere", 2_000_000);
+            int lilliputIndex = countries.FindIndex(x => x.Population < 2_000_000);
+            countries.Insert(lilliputIndex,lilliput);
+
+            //Remove item at idex
+            countries.RemoveAt(lilliputIndex);
+
+            foreach (Country country in countries)
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population).PadLeft(15)}: {country.Name}");
+            }
+            Console.WriteLine($"Total countries: {countries.Count}");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
